a1f70c721e3a5f2b94eaf76765b4b893fa8013d3
yuki-hatakeyama <yuki-hatakeyama@two2.jp>
2020-04-21 00:02:44 +0900

    ディレクトリの変更 + 対象の画像ファイルを追加

--- gulpfile.js
+++ gulpfile.js
@@ -6,22 +6,31 @@ const mozjpeg = require("imagemin-mozjpeg");
 const pngquant = require("imagemin-pngquant");
 const changed = require("gulp-changed");
 
-// srcImgフォルダのjpg,png画像を圧縮して、distImgフォルダに保存する
-gulp.task("default", function () {
+// images フォルダの画像を圧縮して、dist フォルダに保存する
+gulp.task("default", () => {
   return gulp
-    .src("./src/srcImg/**/*.{png,jpg}") // srcImgフォルダ以下のpng,jpg画像を取得する
-    .pipe(changed("distImg")) // srcImg と distImg を比較して異なるものだけ圧縮する
+    .src("src/images/**/*.{jpg,jpeg,png,gif,svg}")
+    .pipe(changed("dist")) // images と dist を比較して、差分のあるものだけを圧縮する
     .pipe(
       imagemin([
         pngquant({
-          quality: "70-85", // 画質
-          speed: 1 // スピード
+          quality: [.7, .85], // 画質
+          speed: 1, // 変換スピードの設定。1 ~ 10 の数値を設定。1 がもっとも高品質
         }),
         mozjpeg({
           quality: 85, // 画質
           progressive: true
         }),
+        imagemin.gifsicle({
+          interlaced: false,
+        }),
+        imagemin.svgo({
+          plugins: [
+            { removeViewBox: true },
+            { cleanupIDs: false },
+          ]
+        }),
       ]),
     )
-    .pipe(gulp.dest("./src/distImg/")); //保存
+    .pipe(gulp.dest("./src/dist/")); //保存
 });
