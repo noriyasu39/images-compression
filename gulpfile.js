528be6cfe36510ffa4837a8d4d0a66df924affe3
noriyasu39 <>
2020-04-22 00:41:10 +0900

    gulp.task を無くし、gulp の処理をアロー関数でおこなうようにに変更 → gulp.task はgulp4 では非推奨であるため。

--- gulpfile.js
+++ gulpfile.js
@@ -6,19 +6,21 @@ const mozjpeg = require("imagemin-mozjpeg");
 const pngquant = require("imagemin-pngquant");
 const changed = require("gulp-changed");
 
-// images フォルダの画像を圧縮して、dist フォルダに保存する
-gulp.task("default", () => {
+/**
+ * 画像圧縮のタスク
+ */
+const imagCompression = () => {
   return gulp
     .src("src/images/**/*.{jpg,jpeg,png,gif,svg}")
-    .pipe(changed("dist")) // images と dist を比較して、差分のあるものだけを圧縮する
+    .pipe(changed(dist)) // images と dist を比較して、差分だけを圧縮する
     .pipe(
       imagemin([
         pngquant({
-          quality: [.7, .85], // 画質
+          quality: [.7, .85], // 画質の指定
           speed: 1, // 変換スピードの設定。1 ~ 10 の数値を設定。1 がもっとも高品質
         }),
         mozjpeg({
-          quality: 85, // 画質
+          quality: 85, // 画質の指定
           progressive: true
         }),
         imagemin.gifsicle({
@@ -33,4 +35,6 @@ gulp.task("default", () => {
       ]),
     )
     .pipe(gulp.dest("./src/dist/")); //保存
-});
+};
+
+exports.default = imagCompression;
